4740b57d068a7700ca95d81bc19414d6722918c8
SergeiPleshkov <azrailomgpro@gmail.com>
2018-11-15 18:47:55 +0300

    release 1.0

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,119 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <el-container>
+    <el-aside width="50%">
+      <div v-for="group in Object.keys(UPPER_BLOCK_BUTTONS)"
+              :key="group"
+              class="block">
+        <div class="title">{{group}}</div>
+        <el-button v-for="(button, index) in UPPER_BLOCK_BUTTONS[group]"
+                   class="button"
+                   :key="button"
+                   :type="isButtonSelected(1, button, group, index)"
+                   @click="handleBtn1(group, button)">{{button}}
+        </el-button>
+      </div>
+      <div class="block">
+        <div class="title">EFFECTIVE STACK SIZE</div>
+        <el-button v-for="(size, index) in EFFECTIVE_STACK_SIZE"
+                   class="button"
+                   :key="size"
+                   :type="isButtonSelected(2, size, null, index)"
+                   @click="handleBtn2(index)">{{size}}
+        </el-button>
+      </div>
+    </el-aside>
+    <el-main>
+      <img :src="path">
+    </el-main>
+  </el-container>
 </template>
 
 <script>
+const EFFECTIVE_STACK_SIZE = ['0-7', '7-10', '10-13', '13-17', '17-21', '21+']
+const UPPER_BLOCK_BUTTONS = {
+  'HU SB': ['ACTION'],
+  'HU BB': [
+    'SB MINRASE',
+    'SB 3x RASE',
+    'SB PUSH',
+    'SB COMPLETE'
+  ],
+  '3MAX BU': ['ACTION'],
+  '3MAX SB': [
+    'BU MINRASE',
+    'BU x3 RAISE',
+    'BU PUSH',
+    'BU LIMP',
+    'BU FOLD'
+  ],
+  '3MAX BB': [
+    'BU MINRASE/SB CALL',
+    'BU MINRASE/SB 3BET PUSH',
+    'BU PUSH/SB CALL',
+    'BU LIMP',
+    'BU LIMP/SB COMPLETE',
+    'BU LIMP/SB ISO RAISE 3BB',
+    'SB MINRASE',
+    'SB 3X RAISE',
+    'SB PUSH',
+    'SB COMPLETE'
+  ],
+}
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      EFFECTIVE_STACK_SIZE,
+      UPPER_BLOCK_BUTTONS,
+      btn1: 'HUSBACTION',
+      btn2: '1'
+    }
+  },
+  methods: {
+    replaceWithEmpty(string){
+      return string.replace(/\s|\//g, '');
+    },
+    handleBtn1(group, button) {
+      this.btn1 = this.replaceWithEmpty(group+button);
+    },
+    handleBtn2(index) {
+      this.btn2 = index+1
+    },
+    isButtonSelected(index, button, group, stackSizeIndex){
+      switch (index) {
+        case 1:
+          return this[`btn${index}`].includes(this.replaceWithEmpty(group+button))
+            ? 'primary'
+            : 'plain';
+        case 2:
+          return stackSizeIndex === this.btn2 - 1
+            ? 'primary'
+            : 'plain';
+      }
+    }
+  },
+  computed: {
+    path() {
+      return require('../assets/' + this.btn1 + '/' + this.btn2 + '.png');
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.block {
+  padding: 15px;
+  display: flex;
+  text-align: left;
+  flex-wrap: wrap;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.button {
+  margin-bottom: 10px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.title {
+  padding: 15px;
+  font-weight: bold;
+  padding-bottom: 10px;
 }
 </style>
